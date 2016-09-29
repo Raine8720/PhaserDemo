9b36706b4f0775c63f03b8e1f027797e78e07d5e
Larman Smith <LarmanSmith@Sontceras-MacBook-Pro.local>
2016-09-29 12:07:59 -0500

    finshed state 0 -sonic-

--- state0.js
+++ state0.js
@@ -1,37 +1,61 @@
-var demo = {}, centerX = 1500/2, centerY = 1000/2, sonic, speed = 4;
+var demo = {}, centerX = 1500/2, centerY = 1000/2, sonic, speed = 6;
 
 demo.state0 = function () {};
 demo.state0.prototype = {
     preload: function () {
-        game.load.image('sonic','assets/sprites/sonicRunCell.png');
+        game.load.spritesheet('sonic','assets/spritesheets/superPeelout.png',198.2,172);
+        game.load.image('greenhill','assets/backgrounds/greenHill.png');
     },
     create: function () {
+        game.physics.startSystem(Phaser.Physics.ARCADE);
         game.stage.backgroundColor = '#0099ff';
         console.log('state0');
         addChangeStateEventListeners();
+        game.world.setBounds(0,0,2813,1000);
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        
+        var greenHill = game.add.sprite(0,0,'greenhill'); 
         
         sonic = game.add.sprite(centerX , centerY, 'sonic');
         sonic.anchor.x = 0.5;
-        sonic.anchor.y = 0.5;
+        sonic.anchor.y = -0.7;
         //
         //or written using
         //sonic.anchor.setTo(0.5,0.5);
+        sonic.scale.setTo(2,2); //scales object
+        
+        game.physics.enable(sonic);
+        sonic.body.collideWorldBounds = true;
+        sonic.animations.add('peelout',[0,1,2,3]);
         
         
+        game.camera.follow(sonic);
+        game.camera.deadzone = new Phaser.Rectangle(centerX-300,0,600,1000);
     },
+    
     update: function () {
         if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+            sonic.scale.setTo(2,2);
             sonic.x +=speed;
+            sonic.animations.play('peelout',25,true);
         }
         else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+            sonic.scale.setTo(-2,2); //flips image going left
             sonic.x -=speed;
+            sonic.animations.play('peelout',25,true);
+        }
+        else{
+            sonic.animations.stop('peelout');
+            sonic.frame =0;
         }
         if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
             sonic.y +=speed;
         }
         else if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
             sonic.y -=speed;
+            if(sonic.y < 300){
+                sonic.y = 300;
+            }
         }
     }
 };
